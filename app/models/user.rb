0b5da145ea703424b1f9f9a2fd1015c66ce04b09
Hammer0369 <akatahama0369@gmail.com>
2021-07-14 09:39:56 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #名前の入力が空の場合はDBに保存しない
-  validates :name, presence: true
+
   # roomsテーブルと中間テーブルのアソシエーション
   has_many :room_users
   has_many :rooms, through: :room_users
   has_many :messages
+  #名前の入力が空の場合はDBに保存しない
+  validates :name, presence: true
 end
