c3a3b9ecfa058383215d9f2531b7b03afb3dc8b2
Hammer0369 <akatahama0369@gmail.com>
2021-07-14 11:30:56 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
     # @messageには、Message.newで生成した、Messageモデルのインスタンス情報を代入
     # @roomには、paramsに含まれているroom_idを代入
     @message = Message.new
-    @room = Room.find(params[:room_id])
+    @room = Room.find(params[:room_id]) # チャットルームに関する情報
     # チャットルームに紐付いている全てのメッセージ（@room.messages）
     @messages = @room.messages.includes(:user) # N+1問題をincludesメソッドで解消→ユーザー情報を1度のアクセスでまとめて取得
   end
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
   def create
     # @room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出し
     # 生成したインスタンスを@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存
-    @room = Room.find(params[:room_id])
+    @room = Room.find(params[:room_id]) # チャットルームに関する情報
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
