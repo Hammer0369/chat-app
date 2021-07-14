370b90673709392781083a977015ae977d214c8e
Hammer0369 <akatahama0369@gmail.com>
2021-07-14 15:45:36 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image # MessagesテーブルとActive Storageのテーブルで管理された画像ファイルのアソシエーション。
   # 「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しないというバリデーションを設定している。
   validates :content, presence: true
 end
